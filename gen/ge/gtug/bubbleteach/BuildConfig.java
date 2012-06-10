e5e423f1fe8f18fbe3af437cdec57171c6ed2b15
Demur Nodia <demuri111@gmail.com>
2012-06-10 10:56:24 +0400

    innitial commit

---
+++ gen/ge/gtug/bubbleteach/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ge.gtug.bubbleteach;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ge/gtug/bubbleteach/BubbleTeachActivity.java
@@ -0,0 +1,13 @@
+package ge.gtug.bubbleteach;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BubbleTeachActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
