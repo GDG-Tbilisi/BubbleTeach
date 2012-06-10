0da59774761d04e6ea41470b79abbfd3d4040be8
unknown <Natalia@Natalia-PC.(none)>
2012-06-10 09:17:35 -0800

    added SecondAct

--- gen/ge/gtug/bubbleteach/R.java
+++ gen/ge/gtug/bubbleteach/R.java
@@ -23,13 +23,19 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f050002;
         public static final int button2=0x7f050004;
+        public static final int floatingimage=0x7f050008;
         public static final int lay=0x7f050000;
+        public static final int layout=0x7f050005;
+        public static final int sec=0x7f050007;
+        public static final int starttranslate=0x7f050006;
         public static final int table=0x7f050001;
         public static final int textView1=0x7f050003;
     }
     public static final class layout {
-        public static final int firstact=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int falling=0x7f030000;
+        public static final int firstact=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int secondact=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
