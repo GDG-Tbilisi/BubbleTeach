53dc32451273f47ce52585b9fc4260c356dd9f12
otar <otar.zakalashvili@gmail.com>
2012-06-10 18:48:12 -0700

    first activity fixed

--- src/ge/gtug/bubbleteach/FirstActivity.java
+++ src/ge/gtug/bubbleteach/FirstActivity.java
@@ -10,7 +10,6 @@ import android.widget.TableRow;
 
 import java.util.Locale;
 
-
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
@@ -18,34 +17,16 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 public class FirstActivity extends Activity {
-	
-	Button but ; 
-	
-	/** Called when the activity is first created. */
 
-	
-	TextToSpeech ttx;
+	Button but;
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.firstact);
 
-	/*	ttx = new TextToSpeech(
-        		FirstActivity.this, 
-        		new TextToSpeech.OnInitListener() {
-			
-			public void onInit(int status) {
-				// TODO Auto-generated method stub
-				if(status !=TextToSpeech.ERROR){
-					ttx.setLanguage(Locale.US);					
-				}
-				
-			}
-		});*/
-
-
-	
-
 		String alphabet = "abcdefghijqlmnopqrstuvwxyz";
+		TableLayout table = (TableLayout)findViewById(R.id.table);
+		
 
 		int pos = 0;
 		for (int i = 0; i < 7; i++) {
@@ -54,58 +35,27 @@ public class FirstActivity extends Activity {
 				if (pos == 26) {
 					break;
 				}
-			    but = new Button(this);
-				but.setOnClickListener(btnListener);
+				but = new Button(this);
+
 				but.setText(Character.toString(alphabet.charAt(pos++)));
 				but.setHeight(80);
 				but.setWidth(70);
 				but.setBackgroundResource(R.drawable.burti);
 				row.addView(but);
-				but.setId(pos);
 				
 				
-				TableLayout table = (TableLayout) findViewById(R.id.table);
-				((ViewGroup)table.getParent()).removeView(table);
+				
+				
+				
 
+				
+
+			}
 			table.addView(row);
-			but.destroyDrawingCache();
 			
-
-		}}
-		
-		
+			
+		}
 
 	}
-	
-	/* @Override
-	    protected void onPause() {
-	    	// TODO Auto-generated method stub
-	    	if(ttx != null){
-	    		ttx.stop();
-	    		ttx.shutdown();  	
-	    	}
-	    	super.onPause();
-	    }*/
-	 
-	 private OnClickListener btnListener = new OnClickListener()
-	    {
-	    
-	    	public void onClick(View v) {
-				 
-	    	/*Button b = (Button)v;
-	    	   String buttonText = b.getText().toString();
-	    		
-	    		ttx.speak(buttonText, TextToSpeech.QUEUE_FLUSH, null);
-				Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE) ;
-				vibe.vibrate(100);
-	    	*/	
-	    		Log.d("message", "value");
-				
-			}
-	    };
 
-			/*
-
-	 */
-		
 }
